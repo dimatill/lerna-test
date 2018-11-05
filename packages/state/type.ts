3fb1ce94b0eb3cddeb3f56717272bb55502803ab
Dmitriy Till <till.dima@gmail.com>
2018-11-05 15:55:29 +0200

    test

--- packages/state/type.ts
+++ packages/state/type.ts
@@ -1,7 +1,7 @@
 import { ObjectType } from 'type-graphql';
 
 import { IState } from './interface';
-import { ICity } from '../city/interface';
+import { ICity } from '@astrumu/test_city/interface';
 
 @ObjectType({ implements: IState })
 export class State implements IState {
