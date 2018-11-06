d000a2f95267a16e9b47eda1f1f027474097bd4b
Dmitriy Till <till.dima@gmail.com>
2018-11-06 18:46:31 +0200

    test

--- packages/models/state/index.ts
+++
@@ -1,9 +0,0 @@
-import * as input from './input';
-import * as stateInterface from './interface';
-import * as type from './type';
-
-export default {
-  interface: stateInterface,
-  input,
-  type
-}
